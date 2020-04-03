d1a7f470f8d0072769048360dbd70b243e196d12
Guilherme Ramos <guilherme.ramos@freiheit.com>
2020-04-03 20:33:39 +0100

    Updated gitnext script

--- scripts/git/gitnext.sh
+++ scripts/git/gitnext.sh
@@ -2,9 +2,7 @@
 
 nextbr=$(awk -F -v '{print $(NF-1)"-v"($NF + 1)}' <<< $(git rev-parse --abbrev-ref HEAD))
 br=$(git rev-parse --abbrev-ref HEAD)
-git branch "$nextbr" master
-git checkout "$nextbr"
 git pull --rebase origin master
-
-[ "$1" = '-r' ] && git rebase "$br"
-
+git branch "$nextbr" "$br"
+git rebase "$nextbr" master
+git checkout "$nextbr"
